fde3272529eed14f00c362bb2c1547655f28358b
mhnaufal <aal.pirates16@gmail.com>
2022-05-07 14:37:13 +0700

    gravity for the player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 50;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 enum GameMode {
     Menu,
     Playing,
@@ -13,6 +17,8 @@ struct Player {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
@@ -33,6 +39,8 @@ impl GameState for State {
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
@@ -58,16 +66,37 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
+        ctx.cls_bg(LIGHTCYAN);
+        ctx.print_color(
+            0,
+            0,
+            RGB::from_u8(0, 0, 0),
+            RGB::from_u8(255, 255, 255),
+            "Press SPACE to fly!",
+        );
 
-        let mut rusty = Player::new(5, 20);
-        rusty.render(ctx);
+        self.frame_time += ctx.frame_time_ms;
 
-        // self.mode = GameMode::End;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     // Game over menu
@@ -102,13 +131,32 @@ impl Player {
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(0, self.y, RED, BLACK, to_cp437('@'));
+        ctx.set(10, self.y, RED, BLACK, to_cp437('X'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        // act like the accelerator
+        self.velocity = self.velocity + 0.2;
+
+        // adds the accelerator to the y position so that it looks like gravity falling effect
+        self.y = self.y + self.velocity as i32;
+        self.x = self.x + 1;
+
+        // rusty goes over top of the window
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        // upward is -y
+        self.velocity = -2.0;
     }
 }
 
 fn main() -> BError {
     // create a terminal instance
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::simple(SCREEN_WIDTH, SCREEN_HEIGHT).unwrap()
         .with_title("Flappy Rusty")
         .with_vsync(false)
         .build()?; // will pass the errors to parent a.k.a main() function
